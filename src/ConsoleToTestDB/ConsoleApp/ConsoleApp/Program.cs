5c736cac63700e7ccc1386c387bb36e459732bd2
Oleksandr Buchynskyi <o.buchynskyi@abtollc.com>
2023-10-18 12:50:44 +0300

    init

---
+++ src/ConsoleToTestDB/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp_DBViewer
+{
+    class Program
+    {
+        private const string ConnectionString = @"Data Source=PC\SQLEXPRESS;Initial Catalog=University;Integrated Security=True;Connect Timeout=30;";
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    GenerateData(connection);
+
+                    PrintTableData(connection, "Users");
+                    PrintTableData(connection, "Boards");
+                    PrintTableData(connection, "UserBoardRoles");
+                    PrintTableData(connection, "Tasks");
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        static void GenerateData(SqlConnection connection)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 40; i++)
+            {
+                string username = $"User{random.Next(1000, 9999)}";
+                string email = $"{username.ToLower()}@example.com";
+                using (SqlCommand cmd = new SqlCommand($"INSERT INTO Users (Username, Email) VALUES (@Username, @Email)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+                string boardName = $"Board{random.Next(1000, 9999)}";
+                using (SqlCommand cmd = new SqlCommand($"INSERT INTO Boards (Name) VALUES (@Name)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", boardName);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            for (int i = 1; i <= 40; i++)
+            {
+                int roleId = random.Next(1, 4);
+                using (SqlCommand cmd = new SqlCommand($"INSERT INTO UserBoardRoles (UserId, BoardId, RoleId) VALUES (@UserId, @BoardId, @RoleId)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@UserId", i);
+                    cmd.Parameters.AddWithValue("@BoardId", i);
+                    cmd.Parameters.AddWithValue("@RoleId", roleId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+                string taskTitle = $"Task{random.Next(1000, 9999)}";
+                string taskDescription = $"Description for {taskTitle}";
+                DateTime deadline = DateTime.Now.AddDays(random.Next(1, 365));
+                using (SqlCommand cmd = new SqlCommand($"INSERT INTO Tasks (Title, Description, Status, Deadline, BoardId) VALUES (@Title, @Description, @Status, @Deadline, @BoardId)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Title", taskTitle);
+                    cmd.Parameters.AddWithValue("@Description", taskDescription);
+                    cmd.Parameters.AddWithValue("@Status", "To Do");
+                    cmd.Parameters.AddWithValue("@Deadline", deadline);
+                    cmd.Parameters.AddWithValue("@BoardId", random.Next(1, 41));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void PrintTableData(SqlConnection connection, string tableName)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand($"SELECT * FROM {tableName}", connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    Console.WriteLine($"=== Data from table: {tableName} ===");
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetName(i)}: {reader.GetValue(i)}; ");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to read data from {tableName}. Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/ConsoleToTestDB/ConsoleApp/ConsoleApp/sql_scripts/create_db_script.sql
@@ -0,0 +1,47 @@
+﻿USE University;
+GO
+
+CREATE TABLE Users
+(
+    UserId INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE Boards
+(
+    BoardId INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL,
+    Description NVARCHAR(1000),
+    OwnerId INT FOREIGN KEY REFERENCES Users(UserId)
+);
+
+CREATE TABLE BoardRoles
+(
+    RoleId INT PRIMARY KEY IDENTITY(1,1),
+    RoleName NVARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE UserBoardRoles
+(
+    UserBoardRoleId INT PRIMARY KEY IDENTITY(1,1),
+    UserId INT FOREIGN KEY REFERENCES Users(UserId),
+    BoardId INT FOREIGN KEY REFERENCES Boards(BoardId),
+    RoleId INT FOREIGN KEY REFERENCES BoardRoles(RoleId),
+    UNIQUE (UserId, BoardId)
+);
+
+CREATE TABLE Tasks
+(
+    TaskId INT PRIMARY KEY IDENTITY(1,1),
+    BoardId INT FOREIGN KEY REFERENCES Boards(BoardId),
+    Title NVARCHAR(255) NOT NULL,
+    Description NVARCHAR(1000),
+    Status NVARCHAR(50) NOT NULL,
+    Deadline DATETIME,
+    AssignedUserId INT FOREIGN KEY REFERENCES Users(UserId)
+);
+
+INSERT INTO BoardRoles (RoleName) VALUES ('Owner');
+INSERT INTO BoardRoles (RoleName) VALUES ('Basic');
+INSERT INTO BoardRoles (RoleName) VALUES ('Reader');
