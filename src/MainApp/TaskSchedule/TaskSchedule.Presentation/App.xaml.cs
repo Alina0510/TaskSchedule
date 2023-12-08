96f2d4c4a891c659fb8540bcc5f1517bbffe2abd
Svitlana Tykholiz <svetatykholiz@gmail.com>
2023-12-08 15:36:32 +0200

    add global exception handler

--- src/MainApp/TaskSchedule/TaskSchedule.Presentation/App.xaml.cs
+++ src/MainApp/TaskSchedule/TaskSchedule.Presentation/App.xaml.cs
@@ -15,7 +15,43 @@ namespace TaskSchedule.Presentation
         {
             log4net.Config.XmlConfigurator.Configure();
             log.Info("        =============  Started Logging  =============        ");
+            SetupExceptionHandling();
             base.OnStartup(e);
+        }
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+            try
+            {
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+            }
+            finally
+            {
+                log.Error(exception);
+            }
         }
     }
 
